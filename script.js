14291cad335ad9103367d320066a3eeb055457b4
Suraj Kushwaha <surajkushwaha022@gmail.com>
2024-11-26 11:56:58 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// flag=0
+// if (flag==0) {
+//     document.getElementById("loginbutton").addEventListener("click",function() {
+//         this.style.backgroundColor="red"    
+//     })
+//     flag=1
+    
+// }
+// if (flag==1) {
+//     document.getElementById("loginbutton").addEventListener("click",function() {
+//         this.style.backgroundColor="yellow"    
+//     })
+
+    
+// }
+// flag=0
+document.getElementById("head").addEventListener("onmouseenter",function() {
+    // document.getElementById("tab1").style.backgroundColor="red"
+    console.log("h111")
+    
+    
+})
+
+
+   
+
+// var toggle=true;
+// function myFunctionkomp1()
+// {
+//     if(flip) { 
+//         document.getElementsById("loginbutton").style.backgroundColor="red";
+//         toggle=false;
+//     }
+//     else {
+//         document.getElementsById("loginbutton").backgroundColor="yellow";
+//         toggle=true;
+//     }
+// }
